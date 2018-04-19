78c3ecf39140b95be0e2208995929b10eee7f761
Alon Gubkin <alon.gubkin@gmail.com>
2018-04-19 20:23:34 +0300

    CR2

--- FavoriteClass/FavoriteClass.py
+++ FavoriteClass/FavoriteClass.py
@@ -6,14 +6,17 @@ class FavoriteAnimal(object):
 
     def birthday(self):
         self.age += 1
+        # birthday() shouldn't return anything, it just increases the age
         return self.age
 
     def get_age(self):
+        #CR: get_age() should just return the age
         return self.birthday()
 
-
+    
 def main():
     dog = FavoriteAnimal()
+    #CR: Use format() for stuff like this, and multiple prints is OK
     print ' '.join([dog.name, 'is', str(dog.age), 'years old.',
                     '\nHappy birthday', dog.name + '.'
                     '\n' + dog.name, 'is now', str(dog.get_age()), 'years old.'])
