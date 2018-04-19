49a660978b5291fe9cdeb4da17080f62819366a8
Alon Gubkin <alon.gubkin@gmail.com>
2018-04-19 19:41:11 +0300

    CR

--- FavoriteClass/FavoriteClass.py
+++ FavoriteClass/FavoriteClass.py
@@ -1,4 +1,4 @@
-
+#CR: All classes should always extend object like this: class MyClass(object)
 class FavoriteAnimal:
 
     def __init__(self):
@@ -10,9 +10,11 @@ class FavoriteAnimal:
         return self.age
 
     def get_age(self):
+        #CR: get_age should return the animal's age and you should do this print in the main() function
         print 'Happy Birthday {} You are now {} years old'.format(self.name, self.birthday())
 
-
+#CR: main function, if __name__ == 'main', .......
+#CR: Print age, then call birthday, then print new age
 dog = FavoriteAnimal()
 dog.get_age()
 
