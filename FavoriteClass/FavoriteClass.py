4b7da32d5d8ba674bb0d724aba5892c05a8312ff
eladgubkin <Eladox3@gmail.com>
2018-04-19 20:49:24 +0300

    added a cat

--- FavoriteClass/FavoriteClass.py
+++ FavoriteClass/FavoriteClass.py
@@ -16,10 +16,14 @@ class FavoriteAnimal(object):
     
 def main():
     dog = FavoriteAnimal('Vafel', 10)
-    print '{} is {} years old.'.format(dog.get_name(), dog.get_age())
-    dog.birthday()
-    print 'Happy birthday {}'.format(dog.get_name())
-    print '{} is now {} years old.'.format(dog.get_name(), dog.get_age())
+    cat = FavoriteAnimal('Oscar', 4)
+    animals = [dog, cat]
+
+    for animal in animals:
+        print '{} is {} years old.'.format(animal.get_name(), animal.get_age())
+        animal.birthday()
+        print 'Happy birthday {}'.format(animal.get_name())
+        print '{} is now {} years old.\n'.format(animal.get_name(), animal.get_age())
 
 
 if __name__ == '__main__':
