c65164d8c55de285acb16505ea412a096aea8a43
eladgubkin <Eladox3@gmail.com>
2018-04-22 00:14:33 +0300

    CR please

--- FavoriteClass/FavoriteClass.py
+++ FavoriteClass/FavoriteClass.py
@@ -1,25 +1,12 @@
-class FavoriteAnimal(object):
+from NewClass import FavoriteAnimal
 
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
 
-    def get_name(self):
-        return self.name
-
-    def birthday(self):
-        self.age += 1
-
-    def get_age(self):
-        return self.age
-
-    
 def main():
-    for animal in FavoriteAnimal('Vafel', 10), FavoriteAnimal('Oscar', 4):
-        print '{} is {} years old.'.format(animal.get_name(), animal.get_age())
-        animal.birthday()
-        print 'Happy birthday {}'.format(animal.get_name())
-        print '{} is now {} years old.\n'.format(animal.get_name(), animal.get_age())
+    animal = FavoriteAnimal('Vafel', 10)
+    # animal prints out __str__
+    print animal
+    animal.set_name()
+    print animal
 
 
 if __name__ == '__main__':
--- FavoriteClass/NewClass.py
+++ FavoriteClass/NewClass.py
@@ -1,3 +1,35 @@
-#from FavoriteClass import *
+class FavoriteAnimal(object):
 
+    def __init__(self, name, age):
+        self.__name = name
+        self.__age = age
 
+    def get_name(self):
+        return self.__name
+
+    def birthday(self):
+        self.__age += 1
+
+    def get_age(self):
+        return self.__age
+
+    def set_name(self):
+        name_check = raw_input("Do you want to change the animal's name? [Yes] [No] ")
+        if name_check.lower() == 'yes':
+            self.__name = raw_input('Enter the name here: ')
+
+        elif name_check.lower() == 'no':
+            self.__name = self.__name
+            print "Very well, Your animal's name is still {}.".format(FavoriteAnimal.get_name(self))
+            quit()
+
+        else:
+            print 'Invalid action - Try again'
+            FavoriteAnimal.set_name(self)
+
+    def __str__(self):
+        print '\n{} is {} years old.'.format(FavoriteAnimal.get_name(self), FavoriteAnimal.get_age(self))
+        FavoriteAnimal.birthday(self)
+        print 'Happy birthday {}.'.format(FavoriteAnimal.get_name(self))
+        return '{} is now {} years old.\n'.format(FavoriteAnimal.get_name(self), FavoriteAnimal.get_age(self))
+        # if last line is not return it will give a TypeError
