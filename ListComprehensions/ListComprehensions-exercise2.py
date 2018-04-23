c9f9da3288f40e922e8e7233a66274c0a9d56f21
eladgubkin <Eladox3@gmail.com>
2018-04-24 01:53:08 +0300

    added more exercises

---
+++ ListComprehensions/ListComprehensions-exercise2.py
@@ -0,0 +1,19 @@
+
+def anti_bb(string):
+    new_string = []
+    for letter in string:
+        if letter not in ['B', 'b']:
+            new_string.append(letter)
+
+    return ''.join(new_string)
+
+
+def anti_b(string):
+    new_string = [letter for letter in string if letter not in ['B', 'b']]
+
+    return ''.join(new_string)
+
+
+print anti_bb('BBbbBbHelloBBBBbbbb')
+
+print anti_b('BBbbHolaBBbb')
