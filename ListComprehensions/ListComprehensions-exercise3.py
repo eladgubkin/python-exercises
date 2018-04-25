a1f9e7452a2191b48dcceb10fb83a419f386bf48
eladgubkin <Eladox3@gmail.com>
2018-04-26 01:08:40 +0300

    New exercises

---
+++ ListComprehensions/ListComprehensions-exercise3.py
@@ -0,0 +1,18 @@
+
+# without list comprehensions:
+
+
+my_list = []
+for i in range(2000, 3201):
+    if i % 7 == 0 and i % 5 != 0:
+        my_list.append(i)
+
+print my_list
+
+
+# with list comprehensions:
+
+
+i = [i for i in range(2000, 3201) if i % 7 == 0 and i % 5 != 0]
+
+print i
